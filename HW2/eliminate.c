c88e832bdd20a2816e7e00d4d609938694dff873
Ryan Barron <barron62@purdue.edu>
2026-01-21 13:56:27 -0500

    Add files via upload

---
+++ HW2/eliminate.c
@@ -0,0 +1,45 @@
+/* You MUST modify this file */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> 
+#include <string.h> 
+
+#ifdef TEST_ELIMINATE
+// 100% of the score
+void eliminate(int n, int k)
+{
+  // allocate an arry of n elements
+  int * arr = malloc(sizeof(* arr) * n);
+  // check whether memory allocation succeeds.
+  // if allocation fails, stop
+  if (arr == NULL)
+    {
+      fprintf(stderr, "malloc fail\n");
+      return;
+    }
+	
+  // Note that from here on, you can access elements of the arr with
+  // expressions like a[i]
+	
+  // initialize all elements
+
+
+  
+  // counting to k,
+  // mark the eliminated element
+  // print the index of the marked element
+  // repeat until only one element is unmarked
+
+
+
+
+  // print the last one
+
+
+
+
+  // release the memory of the array
+  free (arr);
+}
+#endif
