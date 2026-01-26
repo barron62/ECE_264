3ae7a2ade41123c72be2c9e411ccb85b0c396fa2
Ryan Barron <barron62@purdue.edu>
2026-01-26 16:46:45 -0500

    Add files via upload

---
+++ HW3-Starter/integrator.c
@@ -0,0 +1,23 @@
+/* YOU MUST MODIFY THIS FILE */
+// Read "hw3.h" to learn about the two data types: `Range` and `RangeAnswer`
+
+#include "hw3.h"
+
+double integrate1(Range rng)
+{
+  
+  /* Fill in for Part 1 */
+  // calculate the numerical integration of the function func
+  // based on the three attributes (`lowerlimit`, `upperlimit`, and `intervals`) of the type `Range`.
+  // the return value of function `integrate1` should be the numerical integration (return type is double)  
+  return 0.0;
+}
+
+void integrate2(RangeAnswer * rngans)
+{
+  /* Fill in for Part 2 */
+  // run `integrate1` function
+  // take the return value from `integrate1` function 
+  // and assign it to attribute `answer` of the type `RangeAnswer`
+ 
+}
