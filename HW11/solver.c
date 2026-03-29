5ae2c1dd1787f6fcaf7ef0cd8af7c9a186eff428
Ryan Barron <barron62@purdue.edu>
2026-03-29 16:36:43 -0400

    Add files via upload

---
+++ HW11/solver.c
@@ -0,0 +1,29 @@
+// ***
+// *** You MUST modify this file
+// ***
+
+#include "solver.h"
+#include "list.h"
+#include "mazehelper.h"
+#include "path.h"
+
+PathLL* solveMaze(Maze* m) {
+
+    PathLL* successPaths = buildPaths();
+    char* retval = malloc(((m->height * m->width) + 1) * sizeof(char));
+
+    MazePos mp = {.xpos = m->start.xpos, .ypos = m->start.ypos};
+    depthFirstSolve(m, mp, retval, 0, successPaths);
+
+    free(retval);
+
+    return successPaths;
+}
+
+void depthFirstSolve(Maze* m, MazePos curpos, char* path, int step,
+                     PathLL* successPaths) {
+
+    // TODO (Your best bet is to modify a working implementation from HW9)
+
+    return;
+}
